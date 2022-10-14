f399d518a48bf16d1123bb42086c8ef861fdc474
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-10-14 09:18:29 +0300

    Interface example finished

--- index.php
+++ index.php
@@ -1,23 +1,41 @@
 <?php
 
 class Job {
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function work(){
-        $logger = new ConsoleLogger();
         for($i=0; $i<10; $i++){
-            $logger->log($i);
+            $this->logger->log($i);
         }
     }
 }
 
-class ConsoleLogger {
+class ConsoleLogger implements Logger {
     public function log($message){
         echo $message . "\n";
     }
 }
 
+interface Logger {
+    public function log($message);
+}
+
 
 ///////////////////////
 
+class NothingLogger implements Logger {
+    public function log($message){
 
-$job = new Job();
+    }
+}
+
+
+$consoleLogger = new ConsoleLogger();
+$nothingLogger = new NothingLogger();
+$job = new Job($consoleLogger);
 $job->work();
