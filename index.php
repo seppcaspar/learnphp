ca495815b7a0605399815935782e9031c417b1ba
Kaspar Martin Suursalu <kasparsu@gmail.com>
2022-10-14 08:37:40 +0300

    add trait example

--- index.php
+++ index.php
@@ -1,45 +1,26 @@
 <?php
 
 class Person {
-    public $name;
-    public $age;
-    public $gender;
-    public $idCode;
-    public static $id;
+    use HasEyes;
+}
 
-    private function getCentury(){
-        $num = (int) substr($this->idCode, 0, 1);
-        return 1700 + ceil($num/2) * 100;
-        // switch($num) {
-        //     case 1:
-        //     case 2:
-        //         return 1800;
-        //     case 3:
-        //     case 4:
-        //         return 1900;
-        //     case 5:
-        //     case 6:
-        //         return 2000;
-        // }
-    }
+class Dog {
+    use HasEyes;
+}
 
-    public function birthYear(){
-        self::$id;
-        return substr($this->idCode, 1, 2);
-    }
+class Fly {
+    use HasEyes;
+}
 
-    public function getFullYear(){
-        return $this->getCentury() + (int) substr($this->idCode, 1, 2);
-    }
-    public static function getId(){
-        return self::$id;
+trait HasEyes {
+    public $nrOfEyes;
+
+    public function canSeeThings(){
+        return $this->nrOfEyes>0;
     }
 }
-Person::getId();
-Person::$id = 2;
-$kaspar = new Person();
-$kati = new Person();
 
-var_dump(Person::$id);
-var_dump(Person::$id);
-var_dump(Person::$id);
+$person = new Person();
+$person->nrOfEyes = 2;
+var_dump($person->canSeeThings());
+var_dump($person);
